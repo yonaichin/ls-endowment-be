a818070893157a9967b7daf0cae078abd74ee1c3
Olly.W <yonaichin@gmail.com>
2017-03-24 15:14:39 +0800

    read excel file

--- modules/DO1/index.js
+++ modules/DO1/index.js
@@ -1,4 +1,5 @@
 import Promise from 'promise'
+import Excel from 'exceljs'
 
 import { copyToTemp, removeTempFile } from '../../utils'
 
@@ -10,6 +11,17 @@ export const get = () => {
 
     copyToTemp(__dirname + '/product.xlsx', tmpDir + tmpFileName)
     console.log(`${tmpFileName} copied to /tmp`)
+    const workbook = new Excel.Workbook()
+    workbook.xlsx.readFile(tmpDir + tmpFileName)
+      .then(() => {
+        const worksheet = workbook.getWorksheet('工作表1')
+        worksheet.getCell('A1').value = 5
+        const A1 = worksheet.getCell('A1').value
+        const B1 = worksheet.getCell('B1').value
+        console.log('cell', A1, B1)
+        workbook.xlsx.writeFile(tmpDir + 't-' + tmpFileName)
+
+      })
 
     const payload = { tmpFileName }
     resolve(payload)
